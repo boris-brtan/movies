2e73e8ff1f3475fecc69f3dd920c80847f523b7f
Boris Brtáň <39240194+boris-brtan@users.noreply.github.com>
2024-02-16 20:19:48 +0100

    reduce MovieCard list unnecessary rerender

--- src/component/MovieCard.tsx
+++ src/component/MovieCard.tsx
@@ -1,4 +1,4 @@
-import { useCallback } from 'react'
+import { memo, useCallback } from 'react'
 import { useShallow } from 'zustand/react/shallow'
 import { Box, Card, CardActionArea, CardActions, CardContent, IconButton, Typography } from '@mui/material'
 import { Star, StarBorder } from '@mui/icons-material'
@@ -7,33 +7,43 @@ import { useFavoriteStore } from '../store'
 import { useRedirect } from '../util'
 import { Poster } from './Poster'
 
+function FavoriteButton(props: MovieItem) {
+    const [isFavorite, toggleFavorite] = useFavoriteStore(useShallow((state) => [
+        state.isFavorite(props),
+        state.toggleFavorite,
+    ]))
+
+    return <IconButton
+        size="small"
+        color="warning"
+        title="Add to favorites"
+        onClick={() => toggleFavorite(props)}
+    >
+        {isFavorite ? <Star /> : <StarBorder />}
+    </IconButton>
+}
+
 /**
  * Renders item for {@link MovieList} or {@link FavoriteList}.
  *
  * @param item movie or tv show list item data
  */
-export function MovieCard(item: MovieItem) {
-    const [isFavorite, toggleFavorite] = useFavoriteStore(useShallow((state) => [
-        state.isFavorite(item),
-        () => state.toggleFavorite(item),
-    ]))
+export const MovieCard = memo(function MovieCard(props: MovieItem) {
     const redirect = useRedirect()
-    const onCardClick = useCallback(() => redirect('/movie/' + item.imdbID), [item.imdbID, redirect])
+    const onCardClick = useCallback(() => redirect('/movie/' + props.imdbID), [props.imdbID, redirect])
 
     return <Card className="list-item">
         <CardActionArea onClick={onCardClick}>
             <Box sx={{ display: 'flex', flexDirection: 'row' }}>
-                <Poster src={item.Poster} />
+                <Poster src={props.Poster} />
                 <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">{item.Title}</Typography>
-                    <Typography variant="body2" color="text.secondary">{item.Year}</Typography>
+                    <Typography gutterBottom variant="h5" component="div">{props.Title}</Typography>
+                    <Typography variant="body2" color="text.secondary">{props.Year}</Typography>
                 </CardContent>
             </Box>
         </CardActionArea>
         <CardActions>
-            <IconButton size="small" color="warning" title="Add to favorites" onClick={toggleFavorite}>
-                {isFavorite ? <Star /> : <StarBorder />}
-            </IconButton>
+            <FavoriteButton {...props} />
         </CardActions>
     </Card>
-}
+})
